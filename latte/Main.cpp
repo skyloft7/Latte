973e9508ff25b1308d191744b1b13da262f52e2d
skyloft <skyloftmb@gmail.com>
2025-03-08 13:16:31 +0530

    Updated Main.cpp

--- latte/Main.cpp
+++ latte/Main.cpp
@@ -1,24 +1,20 @@
-#include <deque>
-#include <iostream>
-
 #include "core/Mesh.h"
 #include "preview/AccelerationStructPreview.h"
 #include "Core.h"
-//#include "preview/BVHVisualizer.h"
-
 #include "preview/BVHVisualizer.h"
 #include "preview/Visualizer.h"
 
+
+
 int main() {
+
+
 	std::shared_ptr<Mesh> mesh = Core::loadMesh("models/xyzrgb_dragon.obj");
 	std::shared_ptr<Visualizer> visualizer = std::make_shared<BVHVisualizer>();
 
 	AccelerationStructPreview preview;
 
 	preview.run("Stanford Asian Dragon BVH", 960, 540, mesh, visualizer);
-
-
-
 
 	return 0;
 }
