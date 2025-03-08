1d674416024c91a19c6c72005f1641073f7c5161
skyloft <skyloftmb@gmail.com>
2025-03-08 16:09:30 +0530

    Added Imgui to the Visualizer tool

--- latte/preview/AccelerationStructPreview.cpp
+++ latte/preview/AccelerationStructPreview.cpp
@@ -13,59 +13,7 @@
 #include <gtc/type_ptr.hpp>
 #define GLM_ENABLE_EXPERIMENTAL
 #include <gtx/string_cast.hpp>
-#include <numbers>
 
-void APIENTRY glDebugOutput(GLenum source,
-                            GLenum type,
-                            unsigned int id,
-                            GLenum severity,
-                            GLsizei length,
-                            const char *message,
-                            const void *userParam)
-{
-    // ignore non-significant error/warning codes
-    if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
-
-    std::cout << "---------------" << std::endl;
-    std::cout << "Debug message (" << id << "): " <<  message << std::endl;
-
-    switch (source)
-    {
-        case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
-        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
-        case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
-        case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
-        case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
-        case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
-    } std::cout << std::endl;
-
-    switch (type)
-    {
-        case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
-        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
-        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
-        case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
-        case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
-        case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
-        case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
-        case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
-        case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
-    } std::cout << std::endl;
-
-    switch (severity)
-    {
-        case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
-        case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
-        case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
-        case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
-    } std::cout << std::endl;
-    std::cout << std::endl;
-}
-
-
-static void error_callback(int error, const char* description) {
-    std::cerr << "AccelerationStructPreview: " << description << std::endl;
-}
 
 static int createShader(std::string name, std::string source, GLenum shaderType) {
 
@@ -95,42 +43,82 @@ static int createShader(std::string name, std::string source, GLenum shaderType)
 
     return shader;
 }
+static void glfwErrorCallback(int error, const char* description)
+{
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
 
 void AccelerationStructPreview::run(std::string title, int width, int height, std::shared_ptr<Mesh> mesh, std::shared_ptr<Visualizer> visualizer) {
 
 
+    glfwSetErrorCallback(glfwErrorCallback);
+    if (!glfwInit()) {
+        return;
+    }
 
-    GLFWwindow* window;
-    glfwSetErrorCallback(error_callback);
-
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100 (WebGL 1.0)
+    const char* glsl_version = "#version 100";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(IMGUI_IMPL_OPENGL_ES3)
+    // GL ES 3.0 + GLSL 300 es (WebGL 2.0)
+    const char* glsl_version = "#version 300 es";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
 
-    std::string windowTitle = "AccelerationStructPreview [" + title + "]";
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    window = glfwCreateWindow(width, height, windowTitle.c_str(), NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
+
+
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Latte Visualizer Tool", nullptr, nullptr);
+    if (window == nullptr)
+        return;
+
 
     glfwMakeContextCurrent(window);
     gladLoadGL();
     glfwSwapInterval(1);
 
 
-    int flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
-    if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
-    {
-        glEnable(GL_DEBUG_OUTPUT);
-        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-        glDebugMessageCallback(glDebugOutput, nullptr);
-        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
-    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+    ImGui::StyleColorsLight();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    ImVec4 clearColor = ImVec4(0.4, 0.4, 0.4, 1.00f);
+
+
+
+
     glEnable(GL_DEPTH_TEST);
+
 
 
 
@@ -149,11 +137,12 @@ void AccelerationStructPreview::run(std::string title, int width, int height, st
 
         glGenBuffers(1, &modelVbo);
         glBindBuffer(GL_ARRAY_BUFFER, modelVbo);
-        glBufferData(GL_ARRAY_BUFFER, mesh->getVertices()->size() * sizeof(Vertex), &(mesh->getVertices()->at(0)), GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, mesh->getVertices()->size() * sizeof(Vertex), &(mesh->getVertices()->at(0)), GL_STREAM_DRAW);
+
 
         glGenBuffers(1, &modelIbo);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, modelIbo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh->getIndices()->size() * sizeof(int), &(mesh->getIndices()->at(0)), GL_STATIC_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh->getIndices()->size() * sizeof(int), &(mesh->getIndices()->at(0)), GL_STREAM_DRAW);
 
         modelShaderProgram = glCreateProgram();
 
@@ -174,37 +163,37 @@ void AccelerationStructPreview::run(std::string title, int width, int height, st
 
     }
 
-    uint32_t wireframeVao;
-    uint32_t wireframeVbo;
-    uint32_t wireframeIbo;
-    uint32_t wireframeVertexShader;
-    uint32_t wireframeFragmentShader;
-    uint32_t wireframeShaderProgram;
-    std::shared_ptr<Mesh> wireframeGeometry = visualizer->generate(*mesh);
+    uint32_t visualizationVao;
+    uint32_t visualizationVbo;
+    uint32_t visualizationIbo;
+    uint32_t visualizationVertexShader;
+    uint32_t visualizationFragmentShader;
+    uint32_t visualizationShaderProgram;
+    std::shared_ptr<Mesh> visualizationMesh = visualizer->generate(*mesh);
 
 
     {
 
 
-        glGenVertexArrays(1, &wireframeVao);
-        glBindVertexArray(wireframeVao);
+        glGenVertexArrays(1, &visualizationVao);
+        glBindVertexArray(visualizationVao);
 
-        glGenBuffers(1, &wireframeVbo);
-        glBindBuffer(GL_ARRAY_BUFFER, wireframeVbo);
-        glBufferData(GL_ARRAY_BUFFER, wireframeGeometry->getVertices()->size() * sizeof(Vertex), &(wireframeGeometry->getVertices()->at(0)), GL_STATIC_DRAW);
+        glGenBuffers(1, &visualizationVbo);
+        glBindBuffer(GL_ARRAY_BUFFER, visualizationVbo);
+        glBufferData(GL_ARRAY_BUFFER, visualizationMesh->getVertices()->size() * sizeof(Vertex), &(visualizationMesh->getVertices()->at(0)), GL_STREAM_DRAW);
 
-        glGenBuffers(1, &wireframeIbo);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, wireframeIbo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, wireframeGeometry->getIndices()->size() * sizeof(int), &(wireframeGeometry->getIndices()->at(0)), GL_STATIC_DRAW);
+        glGenBuffers(1, &visualizationIbo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, visualizationIbo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, visualizationMesh->getIndices()->size() * sizeof(int), &(visualizationMesh->getIndices()->at(0)), GL_STREAM_DRAW);
 
-        wireframeShaderProgram = glCreateProgram();
+        visualizationShaderProgram = glCreateProgram();
 
-        wireframeVertexShader = createShader("Wireframe_Vert", Core::readFile("shaders/preview/Wireframe.vert"), GL_VERTEX_SHADER);
-        wireframeFragmentShader = createShader("Wireframe_Frag", Core::readFile("shaders/preview/Wireframe.frag"), GL_FRAGMENT_SHADER);
+        visualizationVertexShader = createShader("Visualization_Vert", Core::readFile("shaders/preview/Visualization.vert"), GL_VERTEX_SHADER);
+        visualizationFragmentShader = createShader("Visualization_Frag", Core::readFile("shaders/preview/Visualization.frag"), GL_FRAGMENT_SHADER);
 
-        glAttachShader(wireframeShaderProgram, wireframeVertexShader);
-        glAttachShader(wireframeShaderProgram, wireframeFragmentShader);
-        glLinkProgram(wireframeShaderProgram);
+        glAttachShader(visualizationShaderProgram, visualizationVertexShader);
+        glAttachShader(visualizationShaderProgram, visualizationFragmentShader);
+        glLinkProgram(visualizationShaderProgram);
 
         int strideBytes = 7 * sizeof(float);
 
@@ -224,7 +213,8 @@ void AccelerationStructPreview::run(std::string title, int width, int height, st
 
 
 
-    auto transform = glm::mat4(1.0f) = glm::scale(glm::mat4(1.0), glm::vec3(0.01f));
+    float scale = 0.01;
+    auto transform = glm::mat4(1.0f) = glm::scale(glm::mat4(1.0), glm::vec3(scale));
 
     glm::vec3 whereToLookAt = glm::vec3(0.0f, 0.0f, 0.0f);
     glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
@@ -258,8 +248,35 @@ void AccelerationStructPreview::run(std::string title, int width, int height, st
 
 
     while (!glfwWindowShouldClose(window)) {
+
+        glfwPollEvents();
+        if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0) {
+            ImGui_ImplGlfw_Sleep(10);
+            continue;
+        }
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+
+        ImGui::Begin(title.c_str());
+
+        if (ImGui::SliderFloat("Scale", &scale, 0.0f, 1.0f)) {
+            transform = glm::mat4(1.0f) = glm::scale(glm::mat4(1.0), glm::vec3(scale));
+        }
+
+        ImGui::ColorEdit3("Clear Color", reinterpret_cast<float*> (&clearColor));
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        ImGui::End();
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clearColor.x * clearColor.w, clearColor.y * clearColor.w, clearColor.z * clearColor.w, clearColor.w);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glClearColor(0.4f, 0.4f, 0.4f, 0.0f);
 
 
         //Render the model
@@ -361,39 +378,42 @@ void AccelerationStructPreview::run(std::string title, int width, int height, st
             glDrawElements(GL_TRIANGLES, mesh->getIndices()->size(), GL_UNSIGNED_INT, nullptr);
         }
 
-        //Render the wireframe
+        //Render the visualization
         {
 
-            glBindVertexArray(wireframeVao);
+            glBindVertexArray(visualizationVao);
 
-            glUseProgram(wireframeShaderProgram);
+            glUseProgram(visualizationShaderProgram);
 
-            int transformLocation = glGetUniformLocation(wireframeShaderProgram, "transform");
+            int transformLocation = glGetUniformLocation(visualizationShaderProgram, "transform");
             glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(transform));
 
-            int viewLocation = glGetUniformLocation(wireframeShaderProgram, "view");
+            int viewLocation = glGetUniformLocation(visualizationShaderProgram, "view");
             glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
 
-            int projLocation = glGetUniformLocation(wireframeShaderProgram, "projection");
+            int projLocation = glGetUniformLocation(visualizationShaderProgram, "projection");
             glUniformMatrix4fv(projLocation, 1, GL_FALSE, glm::value_ptr(proj));
 
-            glBindBuffer(GL_ARRAY_BUFFER, wireframeVbo);
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, wireframeIbo);
+            glBindBuffer(GL_ARRAY_BUFFER, visualizationVbo);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, visualizationIbo);
 
 
-            glDrawElements(GL_LINES, wireframeGeometry->getIndices()->size(), GL_UNSIGNED_INT, nullptr);
+            glDrawElements(GL_LINES, visualizationMesh->getIndices()->size(), GL_UNSIGNED_INT, nullptr);
 
 
         }
 
 
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        glfwPollEvents();
         glfwSwapBuffers(window);
     }
 
-    glfwDestroyWindow(window);
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
+    glfwDestroyWindow(window);
     glfwTerminate();
 
 
